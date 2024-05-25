98fc40cf6c4d455ce9b563f518e497bf2ff71b2e
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-05-25 22:21:06 +0200

    add docstring for package

--- table.go
+++ table.go
@@ -1,6 +1,26 @@
 // Copyright (c) 2024 Karl Gaissmaier
 // SPDX-License-Identifier: MIT
 
+// package bart provides a Balanced-Routing-Table (BART).
+//
+// BART is balanced in terms of memory consumption versus
+// lookup time.
+//
+// The lookup time is by a factor of ~2 slower on average as the
+// routing algorithms ART, SMART, CPE, ... but reduces the memory
+// consumption by an order of magnitude in comparison.
+//
+// BART is a multibit-trie with fixed stride length of 8 bits,
+// using the _baseIndex_ function from the ART algorithm to
+// build the complete-binary-tree (CBT) of prefixes for each stride.
+//
+// The second key factor is popcount array compression at each stride level
+// of the CBT prefix tree and backtracking along the CBT in O(k).
+//
+// The CBT is implemented as a bitvector, backtracking is just
+// a matter of fast cache friendly bitmask operations.
+//
+// The child array at each stride level is also popcount compressed.
 package bart
 
 import (
