8e6e4ac1bfa6c2103239018d4dc1f6185b256881
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-08-17 21:13:38 +0200

    simplify

--- allot_tbl.go
+++ allot_tbl.go
@@ -6,23 +6,24 @@ package bart
 import "github.com/bits-and-blooms/bitset"
 
 // allotedPrefixRoutes, overwrite the buffer with the precalculated words in allotment table.
-func allotedPrefixRoutes(idx uint, buf []uint64) {
+func allotedPrefixRoutes(idx uint) (a8 [8]uint64) {
 	if idx < 256 {
-		// overwrite the backing array of bitset with precalculated bitset
-		copy(buf[:], allotLookupTbl[idx][:])
-		return
+		// use precalculated bitset
+		return allotLookupTbl[idx]
 	}
 	// upper half in allot tbl, just 1 bit is set, fast calculation at runtime
-	bitset.From(buf[:]).Set(idx)
+	bitset.From(a8[:]).Set(idx)
+	return a8
 }
 
 // allotedHostRoutes, overwrite the buffer with the precalculated words in allotment table.
-func allotedHostRoutes(idx uint, buf []uint64) {
+func allotedHostRoutes(idx uint) (a4 [4]uint64) {
 	if idx < 256 {
-		// overwrite the backing array of bitset with precalculated bitset
-		copy(buf[:], allotLookupTbl[idx][4:])
-		return
+		// use precalculated bitset
+		copy(a4[:], allotLookupTbl[idx][4:])
+		return a4
 	}
 	// upper half in allot tbl, just 1 bit is set, fast calculation at runtime
-	bitset.From(buf[:]).Set(idx - 256)
+	bitset.From(a4[:]).Set(idx - 256)
+	return a4
 }
