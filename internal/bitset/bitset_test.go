f5036aa92d99c17a6b98554902fd70ef620988f0
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-12-15 13:08:52 +0100

    infrastructure

--- internal/bitset/bitset_test.go
+++ internal/bitset/bitset_test.go
@@ -11,8 +11,7 @@
 // original license:
 // ---------------------------------------------------
 // Copyright 2014 Will Fitzgerald. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// SPDX-License-Identifier: BSD-3-Clause
 // ---------------------------------------------------
 
 package bitset
