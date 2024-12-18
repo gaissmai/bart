f8a1ccb8f90f5c32a29ea9cde78a79b3f11ac9d2
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-12-18 23:13:05 +0100

    rework jsonify after table struct changes

--- jsonify.go
+++ jsonify.go
@@ -19,6 +19,10 @@ type DumpListNode[V any] struct {
 // MarshalJSON dumps the table into two sorted lists: for ipv4 and ipv6.
 // Every root and subnet is an array, not a map, because the order matters.
 func (t *Table[V]) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return nil, nil
+	}
+
 	result := struct {
 		Ipv4 []DumpListNode[V] `json:"ipv4,omitempty"`
 		Ipv6 []DumpListNode[V] `json:"ipv6,omitempty"`
@@ -38,12 +42,18 @@ func (t *Table[V]) MarshalJSON() ([]byte, error) {
 // DumpList4 dumps the ipv4 tree into a list of roots and their subnets.
 // It can be used to analyze the tree or build custom json representation.
 func (t *Table[V]) DumpList4() []DumpListNode[V] {
+	if t == nil {
+		return nil
+	}
 	return t.root4.dumpListRec(0, zeroPath, 0, true)
 }
 
 // DumpList6 dumps the ipv6 tree into a list of roots and their subnets.
 // It can be used to analyze the tree or build custom json representation.
 func (t *Table[V]) DumpList6() []DumpListNode[V] {
+	if t == nil {
+		return nil
+	}
 	return t.root6.dumpListRec(0, zeroPath, 0, false)
 }
 
