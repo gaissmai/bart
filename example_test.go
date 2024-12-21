31c7d5ea720e466ffcfeb7c799afe2b86c977d92
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-12-21 22:57:18 +0100

    better example

--- example_test.go
+++ example_test.go
@@ -29,7 +29,7 @@ var input = []struct {
 	{p("::/0"), a("2001:db8::1")},
 	{p("10.0.1.0/24"), a("10.0.0.0")},
 	{p("169.254.0.0/16"), a("10.0.0.0")},
-	{p("2000::/3"), a("2001:db8::1")},
+	{p("2000::/3"), a("2000::")},
 	{p("2001:db8::/32"), a("2001:db8::1")},
 	{p("127.0.0.0/8"), a("127.0.0.1")},
 	{p("127.0.0.1/32"), a("127.0.0.1")},
@@ -47,15 +47,15 @@ func ExampleTable_Lookup() {
 
 	ip := a("42.0.0.0")
 	value, ok := rtbl.Lookup(ip)
-	fmt.Printf("Lookup: %-20v value: %11v, ok: %v\n", ip, value, ok)
+	fmt.Printf("Lookup: %-20v next-hop: %11v, ok: %v\n", ip, value, ok)
 
 	ip = a("10.0.1.17")
 	value, ok = rtbl.Lookup(ip)
-	fmt.Printf("Lookup: %-20v value: %11v, ok: %v\n", ip, value, ok)
+	fmt.Printf("Lookup: %-20v next-hop: %11v, ok: %v\n", ip, value, ok)
 
 	ip = a("2001:7c0:3100:1::111")
 	value, ok = rtbl.Lookup(ip)
-	fmt.Printf("Lookup: %-20v value: %11v, ok: %v\n", ip, value, ok)
+	fmt.Printf("Lookup: %-20v next-hop: %11v, ok: %v\n", ip, value, ok)
 
 	// Output:
 	// ▼
@@ -71,13 +71,13 @@ func ExampleTable_Lookup() {
 	// ▼
 	// └─ ::/0 (2001:db8::1)
 	//    ├─ ::1/128 (::1%lo)
-	//    ├─ 2000::/3 (2001:db8::1)
+	//    ├─ 2000::/3 (2000::)
 	//    │  └─ 2001:db8::/32 (2001:db8::1)
 	//    └─ fe80::/10 (::1%eth0)
 	//
-	// Lookup: 42.0.0.0             value:  invalid IP, ok: false
-	// Lookup: 10.0.1.17            value:    10.0.0.0, ok: true
-	// Lookup: 2001:7c0:3100:1::111 value: 2001:db8::1, ok: true
+	// Lookup: 42.0.0.0             next-hop:  invalid IP, ok: false
+	// Lookup: 10.0.1.17            next-hop:    10.0.0.0, ok: true
+	// Lookup: 2001:7c0:3100:1::111 next-hop:      2000::, ok: true
 }
 
 func ExampleTable_AllSorted4_callback() {
