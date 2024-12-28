af939b0d4697fed54fe9344a343759e60e412c9c
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2024-12-28 15:02:31 +0100

    impl. Subnets with path compression

--- table_cb_test.go
+++ table_cb_test.go
@@ -46,32 +46,140 @@ func TestSupernetsEdgeCaseCB(t *testing.T) {
 func TestSubnetsCB(t *testing.T) {
 	t.Parallel()
 
-	rtbl := new(Table[any])
-	pfx := mpp("::1/128")
-	rtbl.Subnets(pfx)(func(_ netip.Prefix, _ any) bool {
-		t.Errorf("empty table, must not range over")
-		return false
-	})
+	var zeroPfx netip.Prefix
 
-	val := "foo"
-	rtbl.Insert(pfx, val)
-	rtbl.Subnets(netip.Prefix{})(func(_ netip.Prefix, _ any) bool {
-		t.Errorf("invalid prefix, must not range over")
-		return false
-	})
+	t.Run("empty table", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
 
-	rtbl.Subnets(pfx)(func(p netip.Prefix, v any) bool {
-		if p != pfx {
-			t.Errorf("Subnet(%v), got: %v, want: %v", pfx, p, pfx)
+		rtbl.Subnets(pfx)(func(_ netip.Prefix, _ string) bool {
+			t.Errorf("empty table, must not range over")
 			return false
+		})
+	})
+
+	t.Run("invalid prefix", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+		rtbl.Subnets(zeroPfx)(func(_ netip.Prefix, _ string) bool {
+			t.Errorf("invalid prefix, must not range over")
+			return false
+		})
+	})
+
+	t.Run("identity", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+
+		rtbl.Subnets(pfx)(func(p netip.Prefix, v string) bool {
+			if p != pfx {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, p, pfx)
+			}
+
+			if v != val {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, v, val)
+			}
+			return true
+		})
+	})
+
+	t.Run("default gateway", func(t *testing.T) {
+		want4 := 95_555
+		want6 := 105_555
+
+		rtbl := new(Table[int])
+		for i, pfx := range gimmeRandomPrefixes4(want4) {
+			rtbl.Insert(pfx, i)
+		}
+		for i, pfx := range gimmeRandomPrefixes6(want6) {
+			rtbl.Insert(pfx, i)
 		}
 
-		if v.(string) != val {
-			t.Errorf("Subnet(%v), got: %v, want: %v", pfx, v.(string), val)
-			return false
+		// default gateway v4 covers all v4 prefixes in table
+		dg4 := mpp("0.0.0.0/0")
+		got4 := 0
+		rtbl.Subnets(dg4)(func(_ netip.Prefix, _ int) bool {
+			got4++
+			return true
+		})
+
+		// default gateway v6 covers all v6 prefixes in table
+		dg6 := mpp("::/0")
+		got6 := 0
+		rtbl.Subnets(dg6)(func(_ netip.Prefix, _ int) bool {
+			got6++
+			return true
+		})
+
+		if got4 != want4 {
+			t.Errorf("Subnets v4, want: %d, got: %d", want4, got4)
 		}
-		return true
+		if got6 != want6 {
+			t.Errorf("Subnets v6, want: %d, got: %d", want6, got6)
+		}
 	})
+}
+
+func TestSubnetsCompareCB(t *testing.T) {
+	t.Parallel()
+
+	pfxs := gimmeRandomPrefixes(10_000)
+
+	fast := new(Table[int])
+	gold := goldTable[int]{}
+
+	for i, pfx := range pfxs {
+		fast.Insert(pfx, i)
+		gold.insert(pfx, i)
+	}
+
+	tests := randomPrefixes(200)
+	for _, tt := range tests {
+		gotGold := gold.subnets(tt.pfx)
+		gotFast := []netip.Prefix{}
+
+		fast.Subnets(tt.pfx)(func(p netip.Prefix, _ int) bool {
+			gotFast = append(gotFast, p)
+			return true
+		})
+
+		if !slices.Equal(gotGold, gotFast) {
+			t.Fatalf("Subnets(%q) = %v, want %v", tt.pfx, gotFast, gotGold)
+		}
+	}
+}
+
+func TestSubnetsCompareCB_PC(t *testing.T) {
+	t.Parallel()
+
+	pfxs := gimmeRandomPrefixes(10_000)
+
+	fast := new(Table[int]).WithPathCompression()
+	gold := goldTable[int]{}
+
+	for i, pfx := range pfxs {
+		fast.Insert(pfx, i)
+		gold.insert(pfx, i)
+	}
+
+	tests := randomPrefixes(200)
+	for _, tt := range tests {
+		gotGold := gold.subnets(tt.pfx)
+		gotFast := []netip.Prefix{}
+
+		fast.Subnets(tt.pfx)(func(p netip.Prefix, _ int) bool {
+			gotFast = append(gotFast, p)
+			return true
+		})
+
+		if !slices.Equal(gotGold, gotFast) {
+			t.Fatalf("Subnets(%q) = %v, want %v", tt.pfx, gotFast, gotGold)
+		}
+	}
 }
 
 func TestAll(t *testing.T) {
--- table_iter_test.go
+++ table_iter_test.go
@@ -376,25 +376,203 @@ func TestSupernetsEdgeCase(t *testing.T) {
 func TestSubnets(t *testing.T) {
 	t.Parallel()
 
-	rtbl := new(Table[any])
-	pfx := mpp("::1/128")
-	for range rtbl.Subnets(pfx) {
-		t.Errorf("empty table, must not range over")
-	}
+	var zeroPfx netip.Prefix
 
-	val := "foo"
-	rtbl.Insert(pfx, val)
-	for range rtbl.Subnets(netip.Prefix{}) {
-		t.Errorf("invalid prefix, must not range over")
-	}
+	t.Run("empty table", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
 
-	for p, v := range rtbl.Subnets(pfx) {
-		if p != pfx {
-			t.Errorf("Subnet(%v), got: %v, want: %v", pfx, p, pfx)
+		for range rtbl.Subnets(pfx) {
+			t.Errorf("empty table, must not range over")
+		}
+	})
+
+	t.Run("invalid prefix", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+		for range rtbl.Subnets(zeroPfx) {
+			t.Errorf("invalid prefix, must not range over")
+		}
+	})
+
+	t.Run("identity", func(t *testing.T) {
+		rtbl := new(Table[string])
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+
+		for p, v := range rtbl.Subnets(pfx) {
+			if p != pfx {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, p, pfx)
+			}
+
+			if v != val {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, v, val)
+			}
+		}
+	})
+
+	t.Run("default gateway", func(t *testing.T) {
+		want4 := 95_555
+		want6 := 105_555
+
+		rtbl := new(Table[int])
+		for i, pfx := range gimmeRandomPrefixes4(want4) {
+			rtbl.Insert(pfx, i)
+		}
+		for i, pfx := range gimmeRandomPrefixes6(want6) {
+			rtbl.Insert(pfx, i)
 		}
 
-		if v.(string) != val {
-			t.Errorf("Subnet(%v), got: %v, want: %v", pfx, v.(string), val)
+		// default gateway v4 covers all v4 prefixes in table
+		dg4 := mpp("0.0.0.0/0")
+		got4 := 0
+		for range rtbl.Subnets(dg4) {
+			got4++
+		}
+
+		// default gateway v6 covers all v6 prefixes in table
+		dg6 := mpp("::/0")
+		got6 := 0
+		for range rtbl.Subnets(dg6) {
+			got6++
+		}
+
+		if got4 != want4 {
+			t.Errorf("Subnets v4, want: %d, got: %d", want4, got4)
+		}
+		if got6 != want6 {
+			t.Errorf("Subnets v6, want: %d, got: %d", want6, got6)
+		}
+	})
+}
+
+func TestSubnetsPC(t *testing.T) {
+	t.Parallel()
+
+	var zeroPfx netip.Prefix
+
+	t.Run("empty table", func(t *testing.T) {
+		rtbl := new(Table[string]).WithPathCompression()
+		pfx := mpp("::1/128")
+
+		for range rtbl.Subnets(pfx) {
+			t.Errorf("empty table, must not range over")
+		}
+	})
+
+	t.Run("invalid prefix", func(t *testing.T) {
+		rtbl := new(Table[string]).WithPathCompression()
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+		for range rtbl.Subnets(zeroPfx) {
+			t.Errorf("invalid prefix, must not range over")
+		}
+	})
+
+	t.Run("identity", func(t *testing.T) {
+		rtbl := new(Table[string]).WithPathCompression()
+		pfx := mpp("::1/128")
+		val := "foo"
+		rtbl.Insert(pfx, val)
+
+		for p, v := range rtbl.Subnets(pfx) {
+			if p != pfx {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, p, pfx)
+			}
+
+			if v != val {
+				t.Errorf("Subnet(%v), got: %v, want: %v", pfx, v, val)
+			}
+		}
+	})
+
+	t.Run("default gateway", func(t *testing.T) {
+		want4 := 95_555
+		want6 := 105_555
+
+		rtbl := new(Table[int]).WithPathCompression()
+		for i, pfx := range gimmeRandomPrefixes4(want4) {
+			rtbl.Insert(pfx, i)
+		}
+		for i, pfx := range gimmeRandomPrefixes6(want6) {
+			rtbl.Insert(pfx, i)
+		}
+
+		// default gateway v4 covers all v4 prefixes in table
+		dg4 := mpp("0.0.0.0/0")
+		got4 := 0
+		for range rtbl.Subnets(dg4) {
+			got4++
+		}
+
+		// default gateway v6 covers all v6 prefixes in table
+		dg6 := mpp("::/0")
+		got6 := 0
+		for range rtbl.Subnets(dg6) {
+			got6++
+		}
+
+		if got4 != want4 {
+			t.Errorf("Subnets v4, want: %d, got: %d", want4, got4)
+		}
+		if got6 != want6 {
+			t.Errorf("Subnets v6, want: %d, got: %d", want6, got6)
+		}
+	})
+}
+
+func TestSubnetsCompare(t *testing.T) {
+	t.Parallel()
+
+	pfxs := gimmeRandomPrefixes(10_000)
+
+	fast := new(Table[int])
+	gold := goldTable[int]{}
+
+	for i, pfx := range pfxs {
+		fast.Insert(pfx, i)
+		gold.insert(pfx, i)
+	}
+
+	tests := randomPrefixes(200)
+	for _, tt := range tests {
+		gotGold := gold.subnets(tt.pfx)
+		gotFast := []netip.Prefix{}
+		for pfx := range fast.Subnets(tt.pfx) {
+			gotFast = append(gotFast, pfx)
+		}
+		if !slices.Equal(gotGold, gotFast) {
+			t.Fatalf("Subnets(%q) = %v, want %v", tt.pfx, gotFast, gotGold)
+		}
+	}
+}
+
+func TestSubnetsComparePC(t *testing.T) {
+	t.Parallel()
+
+	pfxs := gimmeRandomPrefixes(10_000)
+
+	fast := new(Table[int]).WithPathCompression()
+	gold := goldTable[int]{}
+
+	for i, pfx := range pfxs {
+		fast.Insert(pfx, i)
+		gold.insert(pfx, i)
+	}
+
+	tests := randomPrefixes(200)
+	for _, tt := range tests {
+		gotGold := gold.subnets(tt.pfx)
+		gotFast := []netip.Prefix{}
+		for pfx := range fast.Subnets(tt.pfx) {
+			gotFast = append(gotFast, pfx)
+		}
+		if !slices.Equal(gotGold, gotFast) {
+			t.Fatalf("Subnets(%q) = %v, want %v", tt.pfx, gotFast, gotGold)
 		}
 	}
 }
