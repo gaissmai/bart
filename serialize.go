cfd05e4c25a572418f5003b56a71577e17fba9f8
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-02-16 14:25:08 +0100

    rearrange and rename

--- serialize.go
+++ serialize.go
@@ -16,11 +16,18 @@ import (
 	"github.com/gaissmai/bart/internal/art"
 )
 
-// kid, a node has no path information about its predecessors,
+// DumpListNode contains CIDR, Value and Subnets, representing the trie
+// in a sorted, recursive representation, especially useful for serialization.
+type DumpListNode[V any] struct {
+	CIDR    netip.Prefix      `json:"cidr"`
+	Value   V                 `json:"value"`
+	Subnets []DumpListNode[V] `json:"subnets,omitempty"`
+}
+
+// trieItem, a node has no path information about its predecessors,
 // we collect this during the recursive descent.
-// The path/depth/idx is needed to get the CIDR back within the trie.
-type kid[V any] struct {
-	// for traversing
+type trieItem[V any] struct {
+	// for traversing, path/depth/idx is needed to get the CIDR back from the trie.
 	n     *node[V]
 	is4   bool
 	path  stridePath
@@ -30,14 +37,6 @@ type kid[V any] struct {
 	// for printing
 	cidr netip.Prefix
 	val  V
-}
-
-// DumpListNode contains CIDR, value and list of subnets (tree childrens).
-// Only used for marshalling.
-type DumpListNode[V any] struct {
-	CIDR    netip.Prefix      `json:"cidr"`
-	Value   V                 `json:"value"`
-	Subnets []DumpListNode[V] `json:"subnets,omitempty"`
 }
 
 // String returns a hierarchical tree diagram of the ordered CIDRs
@@ -50,40 +49,6 @@ func (t *Table[V]) String() string {
 	}
 
 	return w.String()
-}
-
-// MarshalText implements the [encoding.TextMarshaler] interface,
-// just a wrapper for [Table.Fprint].
-func (t *Table[V]) MarshalText() ([]byte, error) {
-	w := new(bytes.Buffer)
-	if err := t.Fprint(w); err != nil {
-		return nil, err
-	}
-
-	return w.Bytes(), nil
-}
-
-// MarshalJSON dumps the table into two sorted lists: for ipv4 and ipv6.
-// Every root and subnet is an array, not a map, because the order matters.
-func (t *Table[V]) MarshalJSON() ([]byte, error) {
-	if t == nil {
-		return nil, nil
-	}
-
-	result := struct {
-		Ipv4 []DumpListNode[V] `json:"ipv4,omitempty"`
-		Ipv6 []DumpListNode[V] `json:"ipv6,omitempty"`
-	}{
-		Ipv4: t.DumpList4(),
-		Ipv6: t.DumpList6(),
-	}
-
-	buf, err := json.Marshal(result)
-	if err != nil {
-		return nil, err
-	}
-
-	return buf, nil
 }
 
 // Fprint writes a hierarchical tree diagram of the ordered CIDRs
@@ -138,6 +103,154 @@ func (t *Table[V]) Fprint(w io.Writer) error {
 	return nil
 }
 
+// MarshalText implements the [encoding.TextMarshaler] interface,
+// just a wrapper for [Table.Fprint].
+func (t *Table[V]) MarshalText() ([]byte, error) {
+	w := new(bytes.Buffer)
+	if err := t.Fprint(w); err != nil {
+		return nil, err
+	}
+
+	return w.Bytes(), nil
+}
+
+// MarshalJSON dumps the table into two sorted lists: for ipv4 and ipv6.
+// Every root and subnet is an array, not a map, because the order matters.
+func (t *Table[V]) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return nil, nil
+	}
+
+	result := struct {
+		Ipv4 []DumpListNode[V] `json:"ipv4,omitempty"`
+		Ipv6 []DumpListNode[V] `json:"ipv6,omitempty"`
+	}{
+		Ipv4: t.DumpList4(),
+		Ipv6: t.DumpList6(),
+	}
+
+	buf, err := json.Marshal(result)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+// DumpList4 dumps the ipv4 tree into a list of roots and their subnets.
+// It can be used to analyze the tree or build the text or json serialization.
+func (t *Table[V]) DumpList4() []DumpListNode[V] {
+	if t == nil {
+		return nil
+	}
+	return t.root4.dumpListRec(0, stridePath{}, 0, true)
+}
+
+// DumpList6 dumps the ipv6 tree into a list of roots and their subnets.
+// It can be used to analyze the tree or build custom json representation.
+func (t *Table[V]) DumpList6() []DumpListNode[V] {
+	if t == nil {
+		return nil
+	}
+	return t.root6.dumpListRec(0, stridePath{}, 0, false)
+}
+
+// dumpListRec, build the data structure rec-descent with the help
+// of getDirectCoveredEntries()
+func (n *node[V]) dumpListRec(parentIdx uint, path stridePath, depth int, is4 bool) []DumpListNode[V] {
+	// recursion stop condition
+	if n == nil {
+		return nil
+	}
+
+	directItems := n.directItemsRec(parentIdx, path, depth, is4)
+
+	// sort the items by prefix
+	slices.SortFunc(directItems, func(a, b trieItem[V]) int {
+		return cmpPrefix(a.cidr, b.cidr)
+	})
+
+	nodes := make([]DumpListNode[V], 0, len(directItems))
+
+	for _, item := range directItems {
+		nodes = append(nodes, DumpListNode[V]{
+			CIDR:  item.cidr,
+			Value: item.val,
+			// build it rec-descent
+			Subnets: item.n.dumpListRec(item.idx, item.path, item.depth, is4),
+		})
+	}
+
+	return nodes
+}
+
+// directItemsRec, returns the direct covered items by parent.
+// It's a complex recursive function, you have to know the data structure
+// by heart to understand this function!
+//
+// See the  artlookup.pdf paper in the doc folder, the baseIndex function is the key.
+func (n *node[V]) directItemsRec(parentIdx uint, path stridePath, depth int, is4 bool) (directItems []trieItem[V]) {
+	// recursion stop condition
+	if n == nil {
+		return nil
+	}
+
+	// for all idx's (mapped prefixes by baseIndex) in this node ...
+	for i, idx := range n.prefixes.All() {
+		// tricky part, skip self, find the next lpm
+		lpm, _, _ := n.lpmGet(idx >> 1)
+
+		// be aware, 0 is here a possible value for parentIdx
+		if lpm == parentIdx {
+			// idx is direct covered
+
+			item := trieItem[V]{
+				n:     n,
+				is4:   is4,
+				path:  path,
+				depth: depth,
+				idx:   idx,
+				//
+				cidr: cidrFromPath(path, depth, is4, idx),
+				val:  n.prefixes.Items[i],
+			}
+
+			directItems = append(directItems, item)
+		}
+	}
+
+	// the node may have childs and path-compressed leaves
+	for i, addr := range n.children.All() {
+		// do a longest-prefix-match, not found returns 0
+		lpm, _, _ := n.lpmGet(art.HostIdx(addr))
+
+		// be aware, 0 is here a possible value for parentIdx
+		if lpm == parentIdx {
+			//
+			switch kid := n.children.Items[i].(type) {
+			case *node[V]: // traverse rec-descent, call with next child node,
+				// record stride path
+				path[depth] = byte(addr)
+
+				// tricky part, set new parentIdx to 0
+				directItems = append(directItems, kid.directItemsRec(0, path, depth+1, is4)...)
+
+			case *leaf[V]: // path-compressed child, stop's recursion for this child
+				item := trieItem[V]{
+					n:    nil,
+					is4:  is4,
+					cidr: kid.prefix,
+					val:  kid.value,
+				}
+
+				directItems = append(directItems, item)
+			}
+		}
+	}
+
+	return directItems
+}
+
 // printNodeRec, the rec-descent tree printer.
 func (dln *DumpListNode[V]) printNodeRec(w io.Writer, pad string) error {
 	// symbols used in tree
@@ -164,117 +277,6 @@ func (dln *DumpListNode[V]) printNodeRec(w io.Writer, pad string) error {
 	}
 
 	return nil
-}
-
-// DumpList4 dumps the ipv4 tree into a list of roots and their subnets.
-// It can be used to analyze the tree or build the text or json serialization.
-func (t *Table[V]) DumpList4() []DumpListNode[V] {
-	if t == nil {
-		return nil
-	}
-	return t.root4.dumpListRec(0, stridePath{}, 0, true)
-}
-
-// DumpList6 dumps the ipv6 tree into a list of roots and their subnets.
-// It can be used to analyze the tree or build custom json representation.
-func (t *Table[V]) DumpList6() []DumpListNode[V] {
-	if t == nil {
-		return nil
-	}
-	return t.root6.dumpListRec(0, stridePath{}, 0, false)
-}
-
-func (n *node[V]) dumpListRec(parentIdx uint, path stridePath, depth int, is4 bool) []DumpListNode[V] {
-	// recursion stop condition
-	if n == nil {
-		return nil
-	}
-
-	directKids := n.getKidsRec(parentIdx, path, depth, is4)
-	slices.SortFunc(directKids, cmpKidByPrefix[V])
-
-	nodes := make([]DumpListNode[V], 0, len(directKids))
-	for _, kid := range directKids {
-		nodes = append(nodes, DumpListNode[V]{
-			CIDR:    kid.cidr,
-			Value:   kid.val,
-			Subnets: kid.n.dumpListRec(kid.idx, kid.path, kid.depth, is4),
-		})
-	}
-
-	return nodes
-}
-
-// getKidsRec, returns the direct kids below path and parentIdx.
-// It's a recursive monster together, you have to know the data structure
-// by heart to understand this function!
-//
-// See the  artlookup.pdf paper in the doc folder, the baseIndex function is the key.
-func (n *node[V]) getKidsRec(parentIdx uint, path stridePath, depth int, is4 bool) []kid[V] {
-	// recursion stop condition
-	if n == nil {
-		return nil
-	}
-
-	var directKids []kid[V]
-
-	for _, idx := range n.prefixes.All() {
-		// parent or self, handled alreday in an upper stack frame.
-		if idx <= parentIdx {
-			continue
-		}
-
-		// check if lpmIdx for this idx' parent is equal to parentIdx
-		lpmIdx, _, _ := n.lpmGet(idx >> 1)
-
-		// if idx is directKid?
-		if lpmIdx == parentIdx {
-			cidr := cidrFromPath(path, depth, is4, idx)
-
-			kid := kid[V]{
-				n:     n,
-				is4:   is4,
-				path:  path,
-				depth: depth,
-				idx:   idx,
-				cidr:  cidr,
-				val:   n.prefixes.MustGet(idx),
-			}
-
-			directKids = append(directKids, kid)
-		}
-	}
-
-	// the node may have childs and leaves, the rec-descent monster starts
-	for i, addr := range n.children.All() {
-		// do a longest-prefix-match
-		lpmIdx, _, _ := n.lpmGet(art.HostIdx(addr))
-		if lpmIdx == parentIdx {
-			switch k := n.children.Items[i].(type) {
-			case *node[V]:
-				path[depth] = byte(addr)
-
-				// traverse, rec-descent call with next child node
-				directKids = append(directKids, k.getKidsRec(0, path, depth+1, is4)...)
-			case *leaf[V]:
-				kid := kid[V]{
-					n:    nil, // path compressed item, stop recursion
-					is4:  is4,
-					cidr: k.prefix,
-					val:  k.value,
-				}
-
-				directKids = append(directKids, kid)
-			}
-		}
-	}
-
-	return directKids
-}
-
-// cmpKidByPrefix, helper function, all prefixes are already normalized (Masked).
-func cmpKidByPrefix[V any](a, b kid[V]) int {
-	return cmpPrefix(a.cidr, b.cidr)
 }
 
 // cmpPrefix, helper function, compare func for prefix sort,
