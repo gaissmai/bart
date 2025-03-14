4adc977f6810541c43aa4114dffaa3a88cbc7811
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-03-14 09:18:08 +0100

    ready to ship

--- lite.go
+++ lite.go
@@ -3,7 +3,14 @@
 
 package bart
 
-import "net/netip"
+import (
+	"net/netip"
+
+	"github.com/gaissmai/bart/internal/art"
+	"github.com/gaissmai/bart/internal/bitset"
+	"github.com/gaissmai/bart/internal/lpmbt"
+	"github.com/gaissmai/bart/internal/sparse"
+)
 
 // Lite is the little sister of [Table]. Lite is ideal for simple
 // IP access-control-lists, a.k.a. longest-prefix matches
@@ -11,31 +18,273 @@ import "net/netip"
 //
 // For all other tasks the much more powerful [Table] must be used.
 type Lite struct {
-	tbl Table[struct{}]
+	// used by -copylocks checker from `go vet`.
+	_ noCopy
+
+	// the root nodes, implemented as popcount compressed multibit tries
+	root4 liteNode
+	root6 liteNode
 }
 
-// Insert adds pfx to the tree.
+// rootNodeByVersion, root node getter for ip version.
+func (l *Lite) rootNodeByVersion(is4 bool) *liteNode {
+	if is4 {
+		return &l.root4
+	}
+	return &l.root6
+}
+
+// Insert adds pfx to the trie.
 func (l *Lite) Insert(pfx netip.Prefix) {
-	l.tbl.Insert(pfx, struct{}{})
+	if !pfx.IsValid() {
+		return
+	}
+
+	// canonicalize prefix
+	pfx = pfx.Masked()
+
+	is4 := pfx.Addr().Is4()
+	n := l.rootNodeByVersion(is4)
+
+	n.insertAtDepth(pfx, 0)
 }
 
-// Delete removes pfx from the tree, pfx does not have to be present.
+// Delete removes pfx from the trie.
 func (l *Lite) Delete(pfx netip.Prefix) {
-	l.tbl.Delete(pfx)
+	if !pfx.IsValid() {
+		return
+	}
+
+	// canonicalize prefix
+	pfx = pfx.Masked()
+
+	// values derived from pfx
+	ip := pfx.Addr()
+	is4 := ip.Is4()
+	bits := pfx.Bits()
+	octets := ip.AsSlice()
+	lastIdx, lastBits := lastOctetIdxAndBits(bits)
+
+	n := l.rootNodeByVersion(is4)
+
+	// record path to deleted node
+	// needed to purge and/or path compress nodes after deletion
+	stack := [maxTreeDepth]*liteNode{}
+
+	// find the trie node
+	for depth, octet := range octets {
+		depth = depth & 0xf // BCE
+
+		if depth > lastIdx {
+			break
+		}
+
+		// push current node on stack for path recording
+		stack[depth] = n
+
+		// delete prefix in trie node
+		if depth == lastIdx {
+			n.prefixes.Clear(art.PfxToIdx(octet, lastBits))
+			n.purgeAndCompress(stack[:depth], octets, is4)
+			return
+		}
+
+		addr := uint(octet)
+		if !n.children.Test(addr) {
+			return
+		}
+		kid := n.children.MustGet(addr)
+
+		// kid is node or leaf at addr
+		switch kid := kid.(type) {
+		case *liteNode:
+			n = kid
+			continue // descend down to next trie level
+
+		case *liteLeaf:
+			// reached a path compressed prefix, stop traversing
+			if kid.prefix != pfx {
+				// nothing to delete
+				return
+			}
+
+			// prefix is equal leaf, delete leaf
+			n.children.DeleteAt(addr)
+			n.purgeAndCompress(stack[:depth], octets, is4)
+
+			return
+
+		default:
+			panic("logic error, wrong node type")
+		}
+	}
+
+	panic("unreachable")
 }
 
 // Contains performs a longest-prefix match for the IP address
 // and returns true if any route matches, otherwise false.
 func (l *Lite) Contains(ip netip.Addr) bool {
-	return l.tbl.Contains(ip)
+	// if ip is invalid, Is4() returns false and AsSlice() returns nil
+	is4 := ip.Is4()
+	n := l.rootNodeByVersion(is4)
+
+	for _, octet := range ip.AsSlice() {
+		addr := uint(octet)
+
+		// for contains, any lpm match is good enough, no backtracking needed
+		if n.lpmTest(art.HostIdx(addr)) {
+			return true
+		}
+
+		// stop traversing?
+		if !n.children.Test(addr) {
+			return false
+		}
+		kid := n.children.MustGet(addr)
+
+		// kid is node or leaf at addr
+		switch kid := kid.(type) {
+		case *liteNode:
+			n = kid
+			continue // descend down to next trie level
+
+		case *liteLeaf:
+			// fringe is the default-route for all nodes below
+			return kid.fringe || kid.prefix.Contains(ip)
+
+		default:
+			panic("logic error, wrong node type")
+		}
+	}
+
+	// invalid IP
+	return false
 }
 
-// String returns a hierarchical tree diagram of the ordered CIDRs as string.
-func (l *Lite) String() string {
-	return l.tbl.String()
+// ###################################################################
+
+// liteNode, see the node struct, but without payload V.
+// Needs less memory and insert and delete is also a bit faster.
+type liteNode struct {
+	prefixes bitset.BitSet256
+	children sparse.Array[any] // [any] is a *liteNode or a *prefixNode
 }
 
-// dumpString is just a wrapper for dump.
-func (l *Lite) dumpString() string {
-	return l.tbl.dumpString()
+// liteLeaf, just a path compressed prefix.
+type liteLeaf struct {
+	prefix netip.Prefix
+	fringe bool
+}
+
+// lpmTest, any longest prefix match
+func (n *liteNode) lpmTest(idx uint) bool {
+	return n.prefixes.IntersectsAny(lpmbt.LookupTbl[idx])
+}
+
+// insertAtDepth, see the similar method for node, but now simpler without payload V.
+func (n *liteNode) insertAtDepth(pfx netip.Prefix, depth int) {
+	ip := pfx.Addr()
+	bits := pfx.Bits()
+	octets := ip.AsSlice()
+	lastIdx, lastBits := lastOctetIdxAndBits(bits)
+
+	// find the proper trie node to insert prefix
+	// start with prefix octet at depth
+	for ; depth < len(octets); depth++ {
+		octet := octets[depth]
+		addr := uint(octet)
+
+		// last significant octet: set prefix idx in node
+		if depth == lastIdx {
+			n.prefixes.Set(art.PfxToIdx(octet, lastBits))
+			return
+		}
+
+		// reached end of trie path ...
+		if !n.children.Test(addr) {
+			// insert prefix as path-compressed leaf
+			n.children.InsertAt(addr, &liteLeaf{pfx, isFringe(depth, bits)})
+			return
+		}
+
+		// ... or decend down the trie
+		kid := n.children.MustGet(addr)
+
+		// kid is node or leaf at addr
+		switch kid := kid.(type) {
+		case *liteNode:
+			n = kid
+			continue // descend down to next trie level
+
+		case *liteLeaf:
+			// reached a path compressed prefix
+			if kid.prefix == pfx {
+				// already exists, nothing to do
+				return
+			}
+
+			// create new node
+			// push the leaf down
+			// insert new child at current leaf position (addr)
+			// descend down, replace n with new child
+			newNode := new(liteNode)
+			newNode.insertAtDepth(kid.prefix, depth+1)
+
+			n.children.InsertAt(addr, newNode)
+			n = newNode
+
+		default:
+			panic("logic error, wrong node type")
+		}
+	}
+
+	panic("unreachable")
+}
+
+// purgeAndCompress, purge empty nodes or compress nodes with single prefix or leaf.
+// similar to the same helper method for node, but without payload V.
+func (n *liteNode) purgeAndCompress(parentStack []*liteNode, childPath []uint8, is4 bool) {
+	// unwind the stack
+	for depth := len(parentStack) - 1; depth >= 0; depth-- {
+		parent := parentStack[depth]
+		addr := uint(childPath[depth])
+
+		prefixCount := n.prefixes.Size()
+		childCount := n.children.Len()
+
+		switch {
+		case prefixCount == 0 && childCount == 0:
+			// just delete this empty node
+			parent.children.DeleteAt(addr)
+
+		case prefixCount == 0 && childCount == 1:
+			// if child is a leaf (not a node), shift it up one level
+			if kid, ok := n.children.Items[0].(*liteLeaf); ok {
+				// delete this node
+				parent.children.DeleteAt(addr)
+
+				// ... insert prefix at parents depth
+				parent.insertAtDepth(kid.prefix, depth)
+			}
+
+		case prefixCount == 1 && childCount == 0:
+			// get prefix back from idx
+			idx, _ := n.prefixes.FirstSet()
+
+			// ... and octet path
+			path := stridePath{}
+			copy(path[:], childPath)
+			pfx := cidrFromPath(path, depth+1, is4, idx)
+
+			// delete this node
+			parent.children.DeleteAt(addr)
+
+			// ... insert prefix at parents depth
+			parent.insertAtDepth(pfx, depth)
+		}
+
+		// climb up the stack
+		n = parent
+	}
 }
