8a3e18a5bddcc711a088798c95c8e986ace867b1
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-09-10 23:30:22 +0200

    run CI also on PR to devel

---
+++ artworstcase_test.go
@@ -0,0 +1,277 @@
+package bart
+
+import (
+	"testing"
+)
+
+func TestArtWorstCaseMatch4(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Contains", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		want := true
+		ok := tbl.Contains(worstCaseProbeIP4)
+		if ok != want {
+			t.Errorf("Contains, worst case match IP4, expected OK: %v, got: %v", want, ok)
+		}
+	})
+
+	t.Run("Lookup", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+		tbl.Insert(ipv4DefaultRoute, ipv4DefaultRoute.String())
+		tbl.Delete(worstCaseProbePfx4)
+
+		wantVal := ipv4DefaultRoute.String()
+		want := true
+		val, ok := tbl.Lookup(worstCaseProbeIP4)
+		if ok != want {
+			t.Errorf("Lookup, worst case match IP4, expected OK: %v, got: %v", want, ok)
+		}
+		if val != wantVal {
+			t.Errorf("Lookup, worst case match IP4, expected: %v, got: %v", wantVal, val)
+		}
+	})
+}
+
+func TestArtWorstCaseMiss4(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Contains", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx4) // delete matching prefix
+
+		want := false
+		ok := tbl.Contains(worstCaseProbeIP4)
+		if ok != want {
+			t.Errorf("Contains, worst case miss IP4, expected OK: %v, got: %v", want, ok)
+		}
+	})
+
+	t.Run("Lookup", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx4) // delete matching prefix
+
+		want := false
+		_, ok := tbl.Lookup(worstCaseProbeIP4)
+		if ok != want {
+			t.Errorf("Lookup, worst case miss IP4, expected OK: %v, got: %v", want, ok)
+		}
+	})
+}
+
+func TestArtWorstCaseMatch6(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Contains", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		want := true
+		ok := tbl.Contains(worstCaseProbeIP6)
+		if ok != want {
+			t.Errorf("Contains, worst case match IP6, expected OK: %v, got: %v", want, ok)
+		}
+	})
+
+	t.Run("Lookup", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+		tbl.Insert(ipv6DefaultRoute, ipv6DefaultRoute.String())
+		tbl.Delete(worstCaseProbePfx6)
+
+		wantVal := ipv6DefaultRoute.String()
+		want := true
+		val, ok := tbl.Lookup(worstCaseProbeIP6)
+		if ok != want {
+			t.Errorf("Lookup, worst case match IP6, expected OK: %v, got: %v", want, ok)
+		}
+		if val != wantVal {
+			t.Errorf("Lookup, worst case match IP6, expected: %v, got: %v", wantVal, val)
+		}
+	})
+}
+
+func TestArtWorstCaseMiss6(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Contains", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx6) // delete matching prefix
+
+		want := false
+		ok := tbl.Contains(worstCaseProbeIP6)
+		if ok != want {
+			t.Errorf("Contains, worst case miss IP6, expected OK: %v, got: %v", want, ok)
+		}
+	})
+
+	t.Run("Lookup", func(t *testing.T) {
+		t.Parallel()
+
+		tbl := new(ArtTable[string])
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx6)
+
+		want := false
+		_, ok := tbl.Lookup(worstCaseProbeIP6)
+		if ok != want {
+			t.Errorf("Lookup, worst case miss IP6, expected OK: %v, got: %v", want, ok)
+		}
+	})
+}
+
+func BenchmarkArtWorstCaseMatch4(b *testing.B) {
+	b.Run("Contains", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		for b.Loop() {
+			tbl.Contains(worstCaseProbeIP4)
+		}
+	})
+
+	b.Run("Lookup", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+		tbl.Insert(ipv4DefaultRoute, ipv4DefaultRoute.String())
+		tbl.Delete(worstCaseProbePfx4)
+
+		for b.Loop() {
+			tbl.Lookup(worstCaseProbeIP4)
+		}
+	})
+}
+
+func BenchmarkArtWorstCaseMiss4(b *testing.B) {
+	b.Run("Contains", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx4) // delete matching prefix
+
+		for b.Loop() {
+			tbl.Contains(worstCaseProbeIP4)
+		}
+	})
+
+	b.Run("Lookup", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP4 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx4) // delete matching prefix
+
+		for b.Loop() {
+			tbl.Lookup(worstCaseProbeIP4)
+		}
+	})
+}
+
+func BenchmarkArtWorstCaseMatch6(b *testing.B) {
+	b.Run("Contains", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		for b.Loop() {
+			tbl.Contains(worstCaseProbeIP6)
+		}
+	})
+
+	b.Run("Lookup", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+		tbl.Insert(ipv6DefaultRoute, ipv6DefaultRoute.String())
+		tbl.Delete(worstCaseProbePfx6)
+
+		for b.Loop() {
+			tbl.Lookup(worstCaseProbeIP6)
+		}
+	})
+}
+
+func BenchmarkArtWorstCaseMiss6(b *testing.B) {
+	b.Run("Contains", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx6) // delete matching prefix
+
+		for b.Loop() {
+			tbl.Contains(worstCaseProbeIP6)
+		}
+	})
+
+	b.Run("Lookup", func(b *testing.B) {
+		tbl := new(ArtTable[string])
+
+		for _, p := range worstCasePfxsIP6 {
+			tbl.Insert(p, p.String())
+		}
+
+		tbl.Delete(worstCaseProbePfx6) // delete matching prefix
+
+		for b.Loop() {
+			tbl.Lookup(worstCaseProbeIP6)
+		}
+	})
+}
