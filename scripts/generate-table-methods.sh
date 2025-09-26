4d7a3ad97838627720e9ac2b83f02c1c6d2b69c4
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-09-26 23:45:43 +0200

    generate table methods

--- scripts/generate-table-methods.sh
+++ scripts/generate-table-methods.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Generate monomorphized methods for all node types from template files.
+# Generate monomorphized methods for all table types from template files.
 
 set -euo pipefail
 
@@ -16,32 +16,33 @@ if [[ ! -f "${template_file}" ]]; then
     DIE  "GOFILE=${GOFILE:-<not set>}" >&2
 fi
 
-INFO "START: Generating monomorphized node methods from template '${GOFILE}' ..."
+INFO "START: Generating monomorphized table methods from template '${GOFILE}' ..."
 
 if grep -q "TODO" "${template_file}"; then
-	DIE "✗ Aborting, found pattern 'TODO' in ${template_file}" >&2
+    DIE "✗ Aborting, found pattern 'TODO' in ${template_file}" >&2
 fi
 
 # Node types to generate
-readonly NODE_TYPES=("bartNode" "fastNode" "liteNode")
+readonly TABLE_TYPES=("Table" "Fast" "liteTable")
 
 # for goimports, see below
 generated_files=()
 
-for nodeType in "${NODE_TYPES[@]}"; do
+for tableType in "${TABLE_TYPES[@]}"; do
     # build output filename e.g. bartiterators_gen.go
-    output_file="${nodeType,,}"                             # lowercase e.g. bartNode -> bartnode
+    output_file="${tableType,,}"                            # lowercase e.g. Fast -> fast
     output_file="${output_file}${template_file/_tmpl/_gen}" # concat with mangled template filename
-    output_file="${output_file//node/}"                     # remove node in filename
-    
+    output_file="${output_file/#table/bart}"       # TODO
+    output_file="${output_file/#litetable/lite}"       # TODO
+
     # Remove go:generate directives and build constraint, add generated header, substitute node type
     sed -e '/^\/\/go:generate\b/d' \
         -e '/Usage:.*go generate\b/d' \
         -e "s|^//go:build ignore.*$|// Code generated from file \"${template_file}\"; DO NOT EDIT.|" \
         -e '/GENERATE DELETE START/,/GENERATE DELETE END/d' \
-        -e "s|_NODE_TYPE|${nodeType}|g" \
+        -e "s|_TABLE_TYPE|${tableType}|g" \
         "${template_file}" > "${output_file}"
-    
+
     if [[ -f "${output_file}" ]]; then
         INFO "✓ Generated ${output_file}"
         generated_files+=("$output_file")
