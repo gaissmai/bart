532540a769db4cb783d0a298b196fea0941007e0
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-09-28 20:19:10 +0200

    preserve ModifyPersist return contract when cloning oldVal

--- barttablemethods_gen.go
+++ barttablemethods_gen.go
@@ -319,14 +319,15 @@ func (t *Table[V]) ModifyPersist(pfx netip.Prefix, cb func(_ V, ok bool) (_ V, d
 	pfx = pfx.Masked()
 
 	oldVal, ok := t.Get(pfx)
+	val := oldVal
 
 	// to clone or not to clone ...
 	cloneFn := cloneFnFactory[V]()
 	if cloneFn != nil && ok {
-		oldVal = cloneFn(oldVal)
+		val = cloneFn(oldVal)
 	}
 
-	newVal, del := cb(oldVal, ok)
+	newVal, del := cb(val, ok)
 
 	switch {
 	case !ok && del: // no-op
