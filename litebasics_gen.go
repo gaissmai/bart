0514c06c4ede4efbd5c23d172787117dc8b4f68a
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-09-28 18:22:50 +0200

    nit

--- litebasics_gen.go
+++ litebasics_gen.go
@@ -356,7 +356,7 @@ func (n *liteNode[V]) delete(pfx netip.Prefix) (val V, exists bool) {
 }
 
 // deletePersist is similar to delete but the receiver isn't modified.
-// All nodes touched during insert are cloned.
+// All nodes touched during delete are cloned.
 func (n *liteNode[V]) deletePersist(cloneFn cloneFunc[V], pfx netip.Prefix) (val V, exists bool) {
 	ip := pfx.Addr() // the pfx must be in canonical form
 	is4 := ip.Is4()
