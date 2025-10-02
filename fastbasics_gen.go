1139b70a26d2a302336385c09a3a1bfef00ef6c9
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-10-02 13:57:33 +0200

    simplify code generation

--- fastbasics_gen.go
+++ fastbasics_gen.go
@@ -1,7 +1,7 @@
+// Code generated from file "nodebasics_tmpl.go"; DO NOT EDIT.
+
 // Copyright (c) 2025 Karl Gaissmaier
 // SPDX-License-Identifier: MIT
-
-// Code generated from file "nodebasics_tmpl.go"; DO NOT EDIT.
 
 package bart
 
