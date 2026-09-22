42b2684a7aea3017cdaa4fefbff584d762a82b2a
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2026-09-22 18:36:59 +0200

    minor: naming

--- internal/nodes/litemethodsgenerated.go
+++ internal/nodes/litemethodsgenerated.go
@@ -1196,15 +1196,15 @@ func (n *LiteNode[V]) DirectItemsRec(parentIdx uint8, path StridePath, depth int
 	for idx, val := range n.AllIndices() {
 		// tricky part, skip self
 		// test with next possible lpm (idx>>1), it's a complete binary tree
-		nextIdx := idx >> 1
+		prevIdx := idx >> 1
 
 		// fast skip, lpm not possible
-		if nextIdx < parentIdx {
+		if prevIdx < parentIdx {
 			continue
 		}
 
 		// do a longest-prefix-match
-		lpm, _, _ := n.LookupIdx(nextIdx)
+		lpm, _, _ := n.LookupIdx(prevIdx)
 
 		// be aware, 0 is here a possible value for parentIdx and lpm (if not found)
 		if lpm == parentIdx {
